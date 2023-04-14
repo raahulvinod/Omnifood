a7faef9d4c9185169f491de5a70bc75150837af9
Rahul vinod <rahulvinod135@gmail.com>
2023-04-14 13:07:31 +0530

    smooth scrolling added

--- js/script.js
+++ js/script.js
@@ -14,6 +14,36 @@ btnNavEl.addEventListener('click', () => {
   headerEl.classList.toggle('nav-open');
 })
 
+//Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // scroll back to top
+    if (href === '#') {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    }
+
+    // scroll to other links 
+    if (href !== "#" && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  })
+})
+
 
 
 
